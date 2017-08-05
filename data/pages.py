c07edc702e03af5256d4de326d2807b6e585af17
Randy Syring <randy@thesyrings.us>
2017-08-05 18:49:11 -0400

    create elders page

---
+++ data/pages.py
@@ -0,0 +1,102 @@
+
+# dict(
+#     name='',
+#     position='',
+#     email='@oakparkbaptist.com',
+#     cdn_ident='-portrait',
+#     content=['''
+#
+#         ''']
+# ),
+
+elders = [
+    dict(
+        name='P. Chase Sears',
+        position='Pastor-Teacher (staff)',
+        email='chase.sears@oakparkbaptist.com',
+        cdn_ident='chase-sears-portrait',
+        content=['''
+                Chase was raised in Bowling Green, KY and came to Oak Park in 2012. He is married to
+                Sarah and has four children: Grace, Hannah, Andrew, and Lillian. Chase came to trust
+                the Lord Jesus during his first semester of college and was discipled in a local
+                church for the next four years. He has seminary degrees from both The Master's
+                Seminary and The Southern Baptist Theological Seminary. Chase desires to see
+                Christians built up by the truths of God's Word, so that they may be equipped
+                to encourage others and make Christ known.
+            ''']
+    ),
+    dict(
+        name='Chris West',
+        position='Pastor of Worship and Young Adults (staff)',
+        email='chris-west@oakparkbaptist.com',
+        cdn_ident='chris-west-portrait',
+        content=['''
+                Chris was raised in Oklahoma, but lived all over the country while serving in the
+                military. After retiring from the Air Force, he came to Southern Indiana in 2016 to
+                attend The Southern Baptist Theological Seminary. Chris has been married to Jennifer
+                since 1994, and they have five children: Brandon, Logan, Madelyn, Allyson and Ryan.
+                Chris trusted Christ at a youth camp in high school and has served in music ministry
+                and various leadership roles ever since. He is currently pursuing a Master of
+                Divinity degree in Worship Leadership. Chris has a passion for leading fellow
+                believers in Christ-centered worship and a strong desire to see young people grow in
+                their faith.
+            ''']
+    ),
+    dict(
+        name='Joshua Page',
+        position='Pastor of Children and Students (staff)',
+        email='joshua.page@oakparkbaptist.com',
+        cdn_ident='joshua-page-portrait',
+        content=['''
+                Joshua is from the south end of Louisville and has attended Oak Park with his wife
+                Angela since 2013. They had their first son, Ezra, in September of 2015. Joshua
+                committed to following Christ at a young age and served faithfully through his early
+                college years at U of L. He lived in rebellion to God for a few years until several
+                circumstances returned him to God's purposes. Eventually the Lord led him to exit
+                the business world so he could serve the church full-time in spring 2015. Joshua now
+                uses his organizational skills to support the church. He possesses a heart for
+                reaching the children and families around Oak Park.
+            ''']
+    ),
+    dict(
+        name='Mike Bauslaugh',
+        position='Lay Pastor',
+        email='mbauslaugh@hotmail.com',
+        cdn_ident='mike-bauslaugh-portrait',
+        content=['''
+                Mike came to Oak Park in 2002 after moving to the area from Iowa with his wife
+                Christine. They have two daughters, two sons-in-law, two grandsons, and two
+                granddaughters. Mike grew up in a Christian family where his parents were both
+                actively involved in the local church. When he was eight years old, he realized that
+                a person does not become a Christian by association, and trusted Jesus Christ as
+                Lord and Savior during an evangelistic crusade at his church in Detroit, Michigan.
+                He is excited to be involved in working with adults, teaching God's Word in
+                Discipleship Classes, and leading the Amazing Grays senior adult ministry. Mike is
+                retired from a position in the transportation industry.
+            ''']
+    ),
+    dict(
+        name='Nathan Hunter',
+        position='Lay Pastor',
+        email='nathan.hunter@oakparkbaptist.com',
+        cdn_ident='nathan-hunter-portrait',
+        content=['''
+            Nathan Hunter was born and raised in Michigan. He came to southern Indiana for
+            theological studies. He came to Christ at an early age thanks to the influence of his
+            parents in building a Godly home. Nathan is especially excited to minister in the areas
+            of teaching and discipleship at Oak Park.
+            ''']
+    ),
+    dict(
+        name='Jim McCalister',
+        position='Lay Pastor',
+        email='jim.mccalister@oakparkbaptist.com',
+        cdn_ident='jim-mccalister-portrait',
+        content=['''
+            Jim and his family (wife: Julie; sons: Hayden & Noah) moved to
+            Sellersburg, IN in 2005. Jim is employed with State Farm currently.  He and his wife
+            serve as leaders in the Oak Park college ministry. As an elder Jim desires to equip the
+            church to carry out the great commission.
+            ''']
+    ),
+]
