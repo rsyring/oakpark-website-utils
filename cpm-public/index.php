e938a5133ad64a6741558b9a202e0e4d4550a85a
Randy Syring <randy@thesyrings.us>
2017-08-05 16:26:46 -0400

    Add files from CPM FTP

---
+++ cpm-public/index.php
@@ -0,0 +1,2 @@
+<?php
+require '/home/oyeme/oyeme/bootstrap.php';
---
+++ cpm-public/js/override.js
@@ -0,0 +1,4 @@
+
+
+// Show OP Tweets on the home page
+$('#home #events').html('<a class="twitter-timeline" data-width="500" data-height="400" data-theme="dark" data-link-color="#9A865B" data-border-color="#9A865B" data-tweet-limit="1" data-chrome="transparent nofooter" href="https://twitter.com/oakpark_jeff">Tweets by oakpark_jeff</a>  <script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>');
